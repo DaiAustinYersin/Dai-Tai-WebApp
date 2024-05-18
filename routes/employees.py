da31117000f7c41a51241f7a1bbd28ee29d6f57c
Nguyen Ngoc Dai <81279113+DaiAustinYersin@users.noreply.github.com>
2024-05-19 00:46:33 +0700

    Revert "Feature/db connector"

--- routes/employees.py
+++ routes/employees.py
@@ -1,9 +1,7 @@
 from flask import render_template
-from app import cursor
 from routes import bp_emp
 
 
 @bp_emp.route('/')
 def index():
-    employees = cursor.execute("SELECT * FROM employees")
-    return render_template('employees/index.html', employees=employees)
+    return render_template('employees/index.html')
